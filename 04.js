d3d7d57520f55d929adba9b23a862305f15b5d73
Bhavin <gusaibhavin88@gmail.com>
2024-11-25 22:51:17 +0530

    second

--- 04.js
+++ 04.js
@@ -1,9 +1,8 @@
-// let keyword 
-// declare variable with let keyword 
+// let keyword
+// declare variable with let keyword
 
 let firstName = "harshit";
 firstName = "Mohit";
 console.log(firstName);
-
 
 // block scope vs funtion scope (covered later in this video)
